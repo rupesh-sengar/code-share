3048f46b18639a85567cb424b842dbd62b96ca88
Rupesh Sengar <rupeshsengar123@gmail.com>
2026-06-19 11:54:47 +0530

    updated socket.ts

--- client/src/utils/socket.ts
+++ client/src/utils/socket.ts
@@ -1,8 +1,10 @@
 // socket.js
 import io from "socket.io-client";
 
-// const socket = io("http://localhost:3001");
-const socket = io("https://code-share-backend.onrender.com/", {
+const LOCAL_URL = "http://localhost:3001";
+const PRD_URL = "https://code-share-backend.onrender.com/"
+//const socket = io("http://localhost:3001");
+const socket = io(LOCAL_URL, {
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: Infinity,
